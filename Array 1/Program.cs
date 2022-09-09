c26b215dc3636d34a6a448d3d594a98f19757703
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-09-09 18:14:06 +0300

    HomeWork20Array1

---
+++ Array 1/Program.cs
@@ -0,0 +1,56 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sum = 0;
+        int mult = 1;
+        int minValue = 1;
+        int maxValue = 10;
+
+        Random random = new Random();
+
+        Console.Write("Введите количество строк: ");
+        int rowNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите длину строки: ");
+        int rowLenth = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите строку для подсчёта суммы: ");
+        int userRow = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите столбец для подсчёта произведения: ");
+        int userColumn = Convert.ToInt32(Console.ReadLine());
+
+        int[,] array = new int[rowNumber, rowLenth];
+
+        int rowForSum = userRow - 1;
+        int columnForMult = userColumn - 1;
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = random.Next(minValue, maxValue);
+                Console.Write($"\b{array[i, j]}  ");
+            }
+            Console.WriteLine();
+        }
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (i == rowForSum)
+                    sum += array[i, j];
+            }
+        }
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (j == columnForMult)
+                    mult *= array[i, j];
+            }
+        }
+
+        Console.WriteLine($"\nСумма {userRow} строки равна {sum}. Произведение {userColumn} столбца равно {mult}.");
+    }
+}
